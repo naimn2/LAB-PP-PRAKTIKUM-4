bdf90e778a36a488b20b91677b9240a18e4cad86
MacBook Air <acimdompla01@gmail.com>
2019-09-12 15:16:21 +0800

    H071191047

---
+++ H071191047/Praktikum1/Bensin.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ class Bensin{
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    double c = 14;
+    double d = a*b;
+    double e = d/c;
+    System.out.println(e);
+    }
+}
