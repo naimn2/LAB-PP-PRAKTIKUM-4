334d2b410650dd391d2bd6d7e9ad1ef160302a21
Muhammad Muflihun Naim <52243639+naimn2@users.noreply.github.com>
2019-09-14 19:18:12 +0700

    Koreksi

--- H071191015/Praktikum1/Praktikum2.java
+++ H071191015/Praktikum1/Praktikum2.java
@@ -10,9 +10,9 @@ class Praktikum2 {
         int sisa1 = detik%3600 ;
 
         int menit = sisa1/60 ;
-        int sisa2 = sisa1%60 ;
+        int sisa2 = sisa1%60 ;      // TIDAK PERLU 2 VARIABEL SISA BISA CUMA 1
 
-        System.out.println(jam + " Jam" + " " + menit + " Menit" + " " + sisa2 + " Detik" );
+        System.out.println(jam + " Jam" + " " + menit + " Menit" + " " + sisa2 + " Detik" );  // FORMAT JAM YANG TEPAT ITU 06:00:00
         input.close();
 
     }
