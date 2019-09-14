67c548ba049556c02d49cd799d4aa9fa1784ee39
Muhammad Muflihun Naim <52243639+naimn2@users.noreply.github.com>
2019-09-14 20:50:59 +0700

    Koreksi

--- H071191044/Praktikum1/no4.java
+++ H071191044/Praktikum1/no4.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
-	class no4 {
+	class no4 {	// ===> PERHATIKAN INDENT AGAR KODE LEBIH RAPI DAN LAIN KALI GUNAKAN TEXT EDITOR
 		public static void main(String [] args) {
 			Scanner x= new Scanner(System.in);
 			int a = x.nextInt();
 			int b = x.nextInt();
-			int c = (b%a);
+			int c = (b%a);	// ===> YANG LEBIH TEPAT "a % b"
 		if (c==0) {
 			System.out.print("Apakah " +a+ " kelipatan dari " +b+ " ? " +(c==0));
 		}else {
