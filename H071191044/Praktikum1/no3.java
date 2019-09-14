fc35df2e179fce37d84e334dd294a5acded2d3ea
Muhammad Muflihun Naim <52243639+naimn2@users.noreply.github.com>
2019-09-14 22:20:46 +0700

    Koreksi

--- H071191044/Praktikum1/no3.java
+++ H071191044/Praktikum1/no3.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 	
-	class no3 {	// ===> PERHATIKAN INDENT AGAR KODE LEBIH RAPI DAN LAIN KALI GUNAKAN TEXT EDITOR
+	class no3 {	// ==> NAMA KELAS HARUS DIAWALI DENGAN HURUF KAPITAL
+		// ===> PERHATIKAN INDENT AGAR KODE LEBIH RAPI DAN LAIN KALI GUNAKAN TEXT EDITOR
 		public static void main(String [] args) {
 			Scanner x= new Scanner (System.in);
 			System.out.print("Ketinggian menara : ");
