6a5616274c01ac9f52f23adb0b612ce4deeb7530
Muhammad Muflihun Naim <52243639+naimn2@users.noreply.github.com>
2019-09-14 20:59:37 +0700

    Koreksi

--- H071191044/Praktikum1/no4.java
+++ H071191044/Praktikum1/no4.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-	class no4 {	// ===> PERHATIKAN INDENT AGAR KODE LEBIH RAPI DAN LAIN KALI GUNAKAN TEXT EDITOR
+	class no4 {	// ===> NAMA CLASS HARUS DIAWALI DENGAN HURUF KAPITAL
+		// ===> PERHATIKAN INDENT AGAR KODE LEBIH RAPI DAN LAIN KALI GUNAKAN TEXT EDITOR
 		public static void main(String [] args) {
 			Scanner x= new Scanner(System.in);
 			int a = x.nextInt();
