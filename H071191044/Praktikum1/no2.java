2a478677ce62b05103d6e65c74a08cd4b6afaa22
Muhammad Muflihun Naim <52243639+naimn2@users.noreply.github.com>
2019-09-14 20:46:03 +0700

    Koreksi

--- H071191044/Praktikum1/no2.java
+++ H071191044/Praktikum1/no2.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
-	class no2 {
+	class no2 {	// ===> PERHATIKAN INDENT AGAR KODE LEBIH RAPI DAN LAIN KALI GUNAKAN TEXT EDITOR
 		public static void main(String [] args) {
 			Scanner x= new Scanner(System.in);
 			System.out.print("Waktu (Detik) : ");
 			int dtk= x.nextInt();
 			int jam= (dtk/3600);
-			int mnt= ((dtk-(jam*3600))/60);
-			int dtkb= (dtk-(jam*3600)-(mnt*60));
-		System.out.print(jam+":"+mnt+":"+dtkb);
+			int mnt= ((dtk-(jam*3600))/60);	// ===> AKAN LEBIH EFISIEN MENGGUNAKAN MODULO
+			int dtkb= (dtk-(jam*3600)-(mnt*60)); // ===> ------------//-----------------
+		System.out.print(jam+":"+mnt+":"+dtkb);  // ===> GUNAKAN "printf()" DAN SESUAIKAN DENGAN FORMAT JAM. CONTOH -> 06:15:00
 		}
 
 }
