096d0ac5aabe2bffcab404e571217d1cc8118aff
Muhammad Muflihun Naim <52243639+naimn2@users.noreply.github.com>
2019-09-14 20:48:33 +0700

    Koreksi

--- H071191044/Praktikum1/no3.java
+++ H071191044/Praktikum1/no3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 	
-	class no3 {
+	class no3 {	// ===> PERHATIKAN INDENT AGAR KODE LEBIH RAPI DAN LAIN KALI GUNAKAN TEXT EDITOR
 		public static void main(String [] args) {
 			Scanner x= new Scanner (System.in);
 			System.out.print("Ketinggian menara : ");
@@ -10,8 +10,8 @@ import java.util.Scanner;
 			System.out.print("Sudut elevasi pengamat dengan ujung belakang kapal : ");
 			double b= x.nextDouble();
 			double pjg;
-				a=Math.toRadians(a);
-				b=Math.toRadians(b);
+				a=Math.toRadians(a);	// ==> PERHATIKAN INDENT
+				b=Math.toRadians(b);	// ==> -------//--------
 			pjg=((Math.tan(a)*h)-(Math.tan(b)*h));
 				System.out.printf("Panjang kapal adalah : %.1f m",pjg);
 				
